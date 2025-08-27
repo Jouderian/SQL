79f378f61246e2619b694ae5831dda1df502ec34
Jouderian Nobre <jouderian@gmail.com>
2025-08-27 15:52:07 -0300

    Mostra a quantidade de funcionarios por situacao

---
+++ resumoFuncionariosPorSituacaoRM.sql
@@ -0,0 +1,20 @@
+--------------------------------------------------------------------------------
+-- Descricao: Mostra a quantidade de funcionarios por situacao
+-- Versao: 1 (27/08/25) Jouderian:
+--------------------------------------------------------------------------------
+
+Select
+  situacoes.DESCRICAO as 'Situacao',
+  Count(*) as 'qtd'
+From PFUNC as funcionarios
+Join PCODSITUACAO as situacoes
+  on situacoes.CODCLIENTE  = funcionarios.CODSITUACAO
+Where funcionarios.CODSITUACAO not in (
+  'D', /* Demitido */
+  'I', /* Apos. por Incapacidade Permanente */
+  'L', /* Licença s/venc */
+  'V', /* Aviso Prévio */
+  'Z'  /* Admissão prox.mês */
+)
+Group by situacoes.DESCRICAO
+Order by Count(*) desc
